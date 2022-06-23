31b0149161866f7c0e9020b847627079b4aed33b
Kavi <ykavi@ebay.com>
2022-06-23 23:07:35 +0300

    detail page ssr

--- components/EmployeeDetail/index.js
+++ components/EmployeeDetail/index.js
@@ -1,24 +1,5 @@
-import { useQuery } from '@apollo/client';
-import { useRouter } from 'next/router';
 import { EmployeeDetailCard } from '@components';
-import { GET_EMPLOYEE_DATA } from '@queries';
 
-const EmployeeDetail = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const { data, loading, error } = useQuery(GET_EMPLOYEE_DATA, {
-    variables: {
-      id: id,
-    },
-  });
-
-  if (loading) <p>Loading...</p>;
-  if (error || !data) return null;
-
-  const { employeeDetail } = data?.employee || {};
-
-  return <EmployeeDetailCard detail={employeeDetail} />;
-};
+const EmployeeDetail = ({ employeeData }) => <EmployeeDetailCard detail={employeeData?.employee?.employeeDetail} />;
 
 export default EmployeeDetail;
--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,5 +1,25 @@
 import { EmployeeDetail } from '@components';
+import client from '@lib';
+import { GET_EMPLOYEE_DATA } from '@queries';
 
-const Detail = () => <EmployeeDetail />;
+const Detail = ({ employeeData }) => <EmployeeDetail employeeData={employeeData} />;
+
+export const getServerSideProps = async (ctx) => {
+  const id = ctx?.query?.id;
+
+  const { data } = await client.query({
+    query: GET_EMPLOYEE_DATA,
+    fetchPolicy: 'no-cache',
+    variables: {
+      id: id,
+    },
+  });
+
+  return {
+    props: {
+      employeeData: data || {},
+    },
+  };
+};
 
 export default Detail;
